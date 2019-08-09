3cd69059fd481930a2d052957ca476005d737c58
PedroManriquez <ptrmanriquez@gmail.com>
2019-08-09 14:43:37 -0400

    Usando todo el proyecto, funcional

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-nav/>
+    <section class="section">
+      <div class="columns is-centered">
+        <router-view/>
+      </div>
+    </section>
+    <app-footer/>
   </div>
 </template>
+
+<script>
+import AppNav from '@/components/AppNav'
+import AppFooter from '@/components/AppFooter'
+export default {
+  name: 'app',
+  components: {
+    AppNav,
+    AppFooter
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
