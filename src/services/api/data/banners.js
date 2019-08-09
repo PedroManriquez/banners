5faec3ad5538457f421e6ec6bfc67637dbb0e32f
PedroManriquez <ptrmanriquez@gmail.com>
2019-08-09 14:40:37 -0400

    Agregado servicio que simula datos desde api

---
+++ src/services/api/data/banners.js
@@ -0,0 +1,44 @@
+export default { //Mock API banners
+  data: {
+    errors: false,
+    msg: 'Datos recuperados',
+    body: [
+      {
+        "id": 1,
+        "name": "Lider - Vuelta a clases",
+        "type": "Mobile",
+        "size": "320x480"
+      },
+      {
+        "id": 2,
+        "name": "Jumbo - Vacaciones invierno",
+        "type": "Desktop",
+        "size": "300x600"
+      },
+      {
+        "id": 3,
+        "name": "Entel - Huawei P30",
+        "type": "Desktop",
+        "size": "300x600"
+      },
+      {
+        "id": 4,
+        "name": "Pepsi - Nueva Light",
+        "type": "Mobile",
+        "size": "320x480"
+      },
+      {
+        "id": 5,
+        "name": "Lays - Más crujientes",
+        "type": "Mobile",
+        "size": "320x480"
+      },
+      {
+        "id": 6,
+        "name": "Ariel - Ropa aún más blanca",
+        "type": "Desktop",
+        "size": "300x600"
+      }
+    ]
+  }
+}
