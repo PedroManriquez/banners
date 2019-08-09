2eecf8ac9f000c56a67c0a31a2d93083ae3feb6c
PedroManriquez <ptrmanriquez@gmail.com>
2019-08-09 14:42:12 -0400

    Agregados componentes de navbar y footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <img class="lobulo-logo" alt="Lóbulo media logo" src="../assets/img/L-BC-ico.png">
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'appFooter'
+}
+</script>
+<style scoped>
+  .lobulo-logo {
+    max-width: 25%;
+    min-width: 10em;
+    opacity: 0.7;
+  }
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar is-fixed-top is-spaced" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="javascript:;">
+        <img src="../assets/img/bulma-logo.png" width="115" height="30">
+      </a>
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" @click="isOpen = !isOpen" v-bind:class="{'is-active': isOpen}">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" v-bind:class="{'is-active': isOpen}">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'appNav',
+  data: () => ({
+    isOpen: false
+  })
+}
+</script>
